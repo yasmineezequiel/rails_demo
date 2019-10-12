6834515d18c33842f2dc7f18383c7fa5aa74917c
yasmine <yasmine.ezequiel@gmail.com>
2019-10-12 20:50:20 +0200

    Eliminates unnecessary migration folder that was causing error.

--- db/schema.rb
+++ db/schema.rb
@@ -31,18 +31,6 @@ ActiveRecord::Schema.define(version: 2019_10_12_181018) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
